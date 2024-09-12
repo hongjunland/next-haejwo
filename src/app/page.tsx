52206f4ed1e5733e5dca9f0727c6020c12ade39d
Hongjun Lee <zunza96@gmail.com>
2024-09-12 14:34:32 +0900

    feat: create 퇴근 시각 계산기 page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,20 @@ import Link from "next/link";
 
 const links = [
   {
-    title: "ㅎ ㅏㄴᄀㅡㄹ -> 한글",
-    description: 'MacOS에서 Windows에 전송된 문서의 파일명을 보정해주는 페이지입니다.',
+    title: "ㅎ ㅏㄴᄀㅡㄹ.xlsx -> 한글.xlsx",
+    description: 'MacOS에서 Windows에 전송된 문서의 깨진 파일명을 보정해주는 페이지입니다.',
     href: "/filename-converter",
   },
   {
-    title: "시간 계산기(Coming Soon!)",
-    description: "선택근무제 시간을 대신 계산해주는 페이지입니다.",
+    title: "퇴근 시각 계산기",
+    description: "퇴근 시간을 대신 계산해주는 페이지입니다.",
     href: "/time-calculator",
   },
+  {
+    title: "미구현 페이지(Coming Soon)",
+    description: "다음 기능은 개발 중입니다",
+    href: "/",
+  }
 ];
 export default function Home() {
   return (
